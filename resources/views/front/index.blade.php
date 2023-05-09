8e06143cfa28a83aaf5805626507d18337a2b3de
dhartupanchal <dhartupanchal@gmail.com>
2023-05-09 21:03:30 +0530

    footer updated

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -129,9 +129,7 @@
     <div class="row">
       <div class="col-md-12">
         <div class="row">
-          <div class="aa-banner-area">
-            <a href="#"><img src="{{asset('front_assets/img/fashion-banner.jpg')}}" alt="fashion banner img"></a>
-          </div>
+         
         </div>
       </div>
     </div>
